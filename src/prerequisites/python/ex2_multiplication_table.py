b47940d4a62e3c237a691192267754422db383c2
Eric Bouchut <ebouchut@gmail.com>
2020-10-11 16:20:11 +0200

    Python: Add docstring for multiplication_table function

--- src/prerequisites/python/ex2_multiplication_table.py
+++ src/prerequisites/python/ex2_multiplication_table.py
@@ -4,14 +4,27 @@
 # - define and call a function
 # - while loop
 
-# Print the `max` first entries of the multiplication table for `n`
-#
-def table_multiplication(n, max=10):
-    print("Table de multiplication de ", n)
+def multiplication_table(n, max=10):
+	  # DocString for this function.
+	  # This is a string aimed to describe what this function does and how to call it.
+	  # It is surrounded with 3 double quotes (`"""`).
+	  #
+	  # Use `help(multiplication_table)` to display this help
+	  #
+    """Print the `max` first entries 
+    of the multiplication table for `n`
+    """
+    print "Multiplication table for ", n
     i = 1
     while (i <= max):
-        print(i, " * ", n, " = ", i*n)
+        print i, " * ", n, " = ", i*n
         i += 1
 
-table_multiplication(5, 20)
-table_multiplication(7)
+# Display the first 10 entries (default max value) for the 7 multiplication table
+multiplication_table(7)
+
+# Display the first 20 entries for the 7 multiplication table
+multiplication_table(5, 20)
+
+# Dislay the docString for the `multiplication_table` function 
+# help(multiplication_table)
